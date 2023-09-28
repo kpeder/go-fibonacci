735e029b1b055a4e2068d59c9b8e9609a1ba5560
kpeder <kpeder@develop>
2023-09-28 16:14:30 -0300

    add tests

---
+++ fibonacci_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSeq(t *testing.T) {
+	flag.Parse()
+	iter := *iter
+
+	//set the control sequence
+	fibonacci := []int{0,1,1,2,3,5,8,13,21,34,55,89}
+
+	//execute the function and report result
+	sequence := Seq(iter, 0, 1)
+
+	//test function generates a sequence of appropriate length and report result
+	if assert.Len(t, sequence, iter) {
+		t.Logf("Seq(%d) PASSED, expected length %d, got %d", iter, iter, len(sequence))
+	} else {
+		t.Errorf("Seq(%d) FAILED, expected length %d, got %d", iter, iter, len(sequence))
+	}
+
+	//test the function generates the Fibonacci sequence and report result
+	if assert.Equal(t, sequence[:min(iter, len(fibonacci))], fibonacci[:min(iter, len(fibonacci))]) {
+		t.Logf("Seq(%d) PASSED, expected sequence %v, got %v", iter, fibonacci[:min(iter, len(fibonacci))], sequence[:min(iter, len(fibonacci))])
+	} else {
+		t.Errorf("Seq(%d) FAILED, expected sequence %v, got %v", iter, fibonacci[:min(iter, len(fibonacci))], sequence[:min(iter, len(fibonacci))])
+	}
+}
